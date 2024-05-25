ab722a2ddc37c0209b4d13df07014e53aed5d048
meenal6051 <meenall6051@gmail.com>
2024-05-25 11:00:19 +0530

    done

--- csi/src/index.js
+++ csi/src/index.js
@@ -8,7 +8,7 @@ import "./styles.css";
 const rootElement = document.getElementById("root");
 ReactDOM.render(<FormComponent />, rootElement);
 
-// // // // If you want to start measuring performance in your app, pass a function
-// // // // to log results (for example: reportWebVitals(console.log))
-// // // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
